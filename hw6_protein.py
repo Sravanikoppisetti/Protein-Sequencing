4f6a74088c61a5ed329080c92747f8769d08610c
sravanikoppisetti <koppisettisravs@msitprogram.com>
2021-11-14 17:05:31 +0530

    Combine Protein List

--- hw6_protein.py
+++ hw6_protein.py
@@ -130,7 +130,12 @@ Parameters: 2D list of strs
 Returns: list of strs
 '''
 def combineProteins(proteinList):
-    return
+    combineProteins=[]
+    for i in proteinList:
+        for j in i:
+            if i not in combineProteins:
+                combineProteins.append(j)
+    return combineProteins
 
 
 '''
@@ -247,7 +252,8 @@ if __name__ == "__main__":
     print("\n" + "#"*15 + " WEEK 2 OUTPUT " + "#" * 15 + "\n")
     runWeek2()
     """
-    test.testCommonProteins()
+    # test.testCommonProteins()
+    test.testCombineProteins()
 
     ## Uncomment these for Week 3 ##
     """
